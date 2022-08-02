933a0424968c5bfce84262f9620bc810e578f021
ahmadfadhilah <stevenasep000@gmail.com>
2022-08-02 09:54:21 +0700

    update

--- App.js
+++ App.js
@@ -1,28 +1,13 @@
-// import React from 'react';
-// import {Provider} from 'react-redux';
-// import store from './src/redux/store';
-// import AppRouter from './src/router/AppRouter';
-
-// const App = () => {
-//   return (
-//     <Provider store={store}>
-//       <AppRouter />
-//     </Provider>
-//   );
-// };
-
-// export default App;
-
-import {View, Text} from 'react-native';
 import React from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
+import AppRouter from './src/router/AppRouter';
 
 const App = () => {
   return (
-    <View>
-      <Text>oiiii</Text>
-      <Icon name="chevron-left" size={20} />
-    </View>
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
   );
 };
 
