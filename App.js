7db981a1c9f363d1c73e22d1427279d8a454a74f
ahmadfadhilah <stevenasep000@gmail.com>
2022-08-02 09:22:43 +0700

    using hermes

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
+// import React from 'react';
+// import {Provider} from 'react-redux';
+// import store from './src/redux/store';
+// import AppRouter from './src/router/AppRouter';
+
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//       <AppRouter />
+//     </Provider>
+//   );
+// };
+
+// export default App;
+
+import {View, Text} from 'react-native';
 import React from 'react';
-import {Provider} from 'react-redux';
-import store from './src/redux/store';
-import AppRouter from './src/router/AppRouter';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
+    <View>
+      <Text>oiiii</Text>
+    </View>
   );
 };
 
