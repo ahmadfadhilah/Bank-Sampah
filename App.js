e0dfbc692c0d1e135b564c7a960fd2fe8d2d10f8
ahmadfadhilah <stevenasep000@gmail.com>
2022-08-11 14:18:57 +0700

    plugin reanimated

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
-// import React from 'react';
-// import {Provider} from 'react-redux';
-// import store from './src/redux/store';
-// import AppRouter from './src/router/AppRouter';
-
-// const App = () => {
-//   return (
-//     <Provider store={store}>
-//       <AppRouter />
-//     </Provider>
-//   );
-// };
-
-// export default App;
-
-import {View, Text} from 'react-native';
 import React from 'react';
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
+import AppRouter from './src/router/AppRouter';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
   );
 };
 
